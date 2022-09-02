9da95766634c0bfa745c136d4b48694ea9efcb9d
Desmond Lambkin <65722547+dLamb17@users.noreply.github.com>
2022-09-01 22:11:50 -0400

    Add files via upload
    first commit - chatbot initialization

---
+++ chatbot.py
@@ -0,0 +1,21 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Creating ChatBot Instance
+chatbot = ChatBot(
+    'CoronaBot',
+    
+    
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch',
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand. I am still learning.',
+            
+        }
+    ]
+)
